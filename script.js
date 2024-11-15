0016f7babe97e640aac2e84a38ceb2c0cf9e7143
Neezybecker <eliezer.nassa@yahoo.fr>
2024-11-15 13:38:38 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ajout d'un effet de défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
